11b2529ec1b48195e424d2405b5bcd9bac9797fb
cjlangan <connorjameslangan@gmail.com>
2025-02-01 12:44:43 -0600

    Logic is now working

--- jhtree.go
+++ jhtree.go
@@ -13,7 +13,6 @@ func main() {
         return;
     }
     directory := os.Args[1];
-    fmt.Println("Analyzing directory..");
 
     // Read the contents of the directory
     files, err := os.ReadDir(directory);
@@ -22,10 +21,13 @@ func main() {
         return;
     }
 
+    // Hashmap to store all base nodes
+    graph := map[string]*Node{};
+
     // Iterate over the files in the directory
     for _, file := range files {
         if !file.IsDir() {
-            fmt.Printf("Found file %s\n", file.Name());
+            //fmt.Printf("Found file %s\n", file.Name());
             nameLen := len(file.Name());
 
             // Last 5 chars of file name
@@ -45,12 +47,72 @@ func main() {
                 // Determine the class and parent
                 lines := strings.Split(string(content), "\n");
                 class, parent := getClassAndParent(lines);
-                fmt.Printf("Class: %s\tParent: %s\n", class, parent);
 
-                // Add them to hierarchy
+                // proceed if there is a class
+                if class != "" {
+                    //fmt.Printf("Class: %s\tParent: %s\n", class, parent);
+                    var child *Node;
+
+                    // Create and add the child if doesn't exist
+                    if graph[class] == nil {
+                        child = &Node{class: class, hasParent: false, subs: []*Node{}};
+                        graph[class] = child
+                    } else {
+                        child = graph[class];
+                    }
+                    
+                    if parent != "" {
+                        // Make child have parent
+                        child.hasParent = true;
+
+                        // Add child to parent
+                        if graph[parent] == nil {
+                            super := &Node{class: parent, hasParent: false, subs: []*Node{}};
+                            graph[parent] = super;
+                        }
+                        graph[parent].subs = append(graph[parent].subs, child);
+                    }
+                }
             }
         }
     }
+    // Print the nodes
+    for _, node := range graph {
+        if(!node.hasParent) {
+            node.printNode(0);
+        }
+    }
+}
+
+// Print node, then its children
+func (n *Node) printNode(indent int) {
+    fmt.Print("─");
+    fmt.Println(n.class);
+
+    // Print children
+    i := 0;
+    for _, node := range n.subs {
+        // Spacing
+        for j := 0; j < indent + 1; j++ {
+            fmt.Print(" ");
+        }
+
+        // determine if last
+        if i == len(n.subs) - 1 {
+            fmt.Print("└");
+        } else {
+            fmt.Print("├");
+        }
+        node.printNode(indent + 1);
+        i++;
+    }
+}
+
+// A node has a class, maybe a parent, and a list of subclasses
+type Node struct {
+    class string;
+    hasParent bool;
+    subs []*Node;
 }
 
 // determines the class and parent class of a java file
