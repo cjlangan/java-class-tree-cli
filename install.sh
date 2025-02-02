45e4b7156e5d68388da52adb929d281db9ec6dcb
cjlangan <connorjameslangan@gmail.com>
2025-02-01 19:03:10 -0600

    Added install script

---
+++ install.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+echo "Downloading myprogram..."
+curl -L -o jhtree https://github.com/cjlangan/java-class-tree-cli/releases/latest/download/jhtree-linux
+chmod +x myprogram
+sudo mv myprogram /usr/local/bin/
+echo "Installation complete! Run 'jhtree <directory>' to start."
